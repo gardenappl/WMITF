33a019dac2a5c47ef4b41f20d088919a60c25379
goldenapple3 <goldenappleminecraft@gmail.com>
2017-01-10 15:25:57 +0200

    Initial commit!

---
+++ WMITF.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WMITF
+{
+	public class WMITF : Mod
+	{
+		public WMITF()
+		{
+			Properties = new ModProperties
+			{
+				Autoload = true
+			};
+		}
+		
+		public class TooltipTweak : GlobalItem
+		{
+			public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+			{
+				if(item.modItem != null && !item.name.Contains("[" + item.modItem.mod.Name + "]") && !item.name.Contains("[" + item.modItem.mod.DisplayName + "]"))
+				{
+					var line = new TooltipLine(mod, "WhichMod", "[" + item.modItem.mod.DisplayName + "]");
+					line.overrideColor = Colors.RarityBlue;
+					tooltips.Add(line);
+				}
+			}
+		}
+	}
+}

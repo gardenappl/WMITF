82115611ce4b1b73c5d8ca6342e1f4c45b0b4c49
Pop000100 <Pop000100@outlook.com>
2020-03-29 14:18:07 -0800

    Add ModConfig, Seperated the file into its classes.
     - Added Mod Config with migration based on Miscellania
     - Made the mod more similar to the other mods by seperating it into is constituant classes

---
+++ ModConfig.cs
@@ -0,0 +1,121 @@
+﻿using log4net;
+using System.IO;
+using Terraria;
+using Terraria.IO;
+using Terraria.ModLoader.Config;
+using System.ComponentModel;
+
+namespace WMITF
+{
+
+    public class Config : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Label("$Mods.WMITF.DisplayWorldTooltips")]
+		[Tooltip("$Mods.WMITF.DisplayWorldTooltips.Desc")]
+		[DefaultValue(false)]
+        public bool DisplayWorldTooltips;
+
+        [Label("$Mods.WMITF.DisplayItemTooltips")]
+		[Tooltip("$Mods.WMITF.DisplayItemTooltips.Desc")]
+		[DefaultValue(true)]
+        public bool DisplayItemTooltips;
+		
+        [Label("$Mods.WMITF.DisplayTechnicalNames")]
+		[Tooltip("$Mods.WMITF.DisplayTechnicalNames.Desc")]
+		[DefaultValue(false)]
+        public bool DisplayTechnicalNames;
+
+	}
+
+    public class LegacyConfig
+    {
+
+        static bool DisplayWorldTooltips = false;
+        const string DisplayWorldTooltipsKey = "DisplayWorldTooltips";
+        static bool DisplayItemTooltips = true;
+        const string DisplayItemTooltipsKey = "DisplayItemTooltips";
+        static bool DisplayTechnicalNames = false;
+        const string DisplayTechnicalNamesKey = "DisplayTechnicalNames";
+
+
+        static string ConfigPath = Path.Combine(Main.SavePath, "Mod Configs", "WMITF.json");
+        static string OldConfigFolderPath = Path.Combine(Main.SavePath, "Mod Configs", "WMITF");
+        static string OldConfigPath = Path.Combine(OldConfigFolderPath, "config.json");
+        static string OldConfigVersionPath = Path.Combine(OldConfigFolderPath, "config.version");
+
+        static readonly Preferences Settings = new Preferences(ConfigPath);
+
+        //Make our own logger because it's too early in the mod load process to use VanillaTweaks.Instance
+        static ILog Logger = LogManager.GetLogger("WMITFConfig");
+
+        public static void Load()
+        {
+            if (Directory.Exists(OldConfigFolderPath))
+            {
+                if (File.Exists(OldConfigPath))
+                {
+                    if (File.Exists(ConfigPath))
+                    {
+                        Logger.Warn("Found config file in old folder! Deleting...");
+                        File.Delete(OldConfigPath);
+                    }
+                    else
+                    {
+                        Logger.Warn("Found config file in old folder! Moving config...");
+                        File.Move(OldConfigPath, ConfigPath);
+                    }
+                }
+                if (File.Exists(OldConfigVersionPath))
+                    File.Delete(OldConfigVersionPath);
+                if (Directory.GetFiles(OldConfigFolderPath).Length == 0 && Directory.GetDirectories(OldConfigFolderPath).Length == 0)
+                    Directory.Delete(OldConfigFolderPath);
+                else
+                    Logger.Warn("Old config folder still cotains some files/directories. They will not get deleted.");
+            }
+
+            if (!File.Exists(ConfigPath))
+                return;
+
+            if (!ReadConfig())
+            {
+                Logger.Warn("Failed to read legacy config file! Deleting...");
+                File.Delete(ConfigPath);
+                return;
+            }
+            MoveToNewFormat();
+        }
+
+        public static bool ReadConfig()
+        {
+            if (Settings.Load())
+            {
+                Settings.Get(DisplayWorldTooltipsKey, ref DisplayWorldTooltips);
+                Settings.Get(DisplayItemTooltipsKey, ref DisplayItemTooltips);
+                Settings.Get(DisplayTechnicalNamesKey, ref DisplayTechnicalNames);
+                return true;
+            }
+            return false;
+        }
+
+        static void MoveToNewFormat()
+        {
+            Logger.Warn("Migrating to new format...");
+
+            var newConfigPath = Path.Combine(ConfigManager.ModConfigPath,
+                    nameof(WMITF) + "_Config" + ".json");
+            var newConfig = new Preferences(newConfigPath);
+            newConfig.Put(DisplayWorldTooltipsKey, DisplayWorldTooltips);
+            newConfig.Put(DisplayItemTooltipsKey, DisplayItemTooltips);
+            newConfig.Put(DisplayTechnicalNamesKey, DisplayTechnicalNames);
+            newConfig.Save();
+
+            if (!File.Exists(newConfigPath))
+                File.Move(ConfigPath, newConfigPath);
+            else
+                File.Delete(ConfigPath);
+        }
+    }
+}
+
---
+++ WorldTooltips.cs
@@ -0,0 +1,96 @@
+﻿
+using System;
+using Terraria;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace WMITF
+{
+    public class WorldTooltips : ModPlayer
+    {
+
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (WMITF.ToggleTooltipsHotkey.JustPressed)
+            {
+                if (ModContent.GetInstance<Config>().DisplayWorldTooltips)
+                {
+                    ModContent.GetInstance<Config>().DisplayWorldTooltips = false;
+                    Main.NewText(Language.GetTextValue("Mods.WMITF.WorldTooltipsOff"));
+                }
+                else
+                {
+                    ModContent.GetInstance<Config>().DisplayWorldTooltips = true;
+                    Main.NewText(Language.GetTextValue("Mods.WMITF.WorldTooltipsOn"));
+                }
+            }
+            if (WMITF.TechnicalNamesHotkey.JustPressed)
+            {
+                if (ModContent.GetInstance<Config>().DisplayTechnicalNames)
+                {
+                    ModContent.GetInstance<Config>().DisplayTechnicalNames = false;
+                    Main.NewText(Language.GetTextValue("Mods.WMITF.TechNamesOff"));
+                }
+                else
+                {
+                    ModContent.GetInstance<Config>().DisplayTechnicalNames = true;
+                    Main.NewText(Language.GetTextValue("Mods.WMITF.TechNamesOn"));
+                }
+            }
+        }
+
+        public override void PostUpdate()
+        {
+            if (Main.dedServ || !ModContent.GetInstance<Config>().DisplayWorldTooltips)
+                return;
+            WMITF.MouseText = String.Empty;
+            WMITF.SecondLine = false;
+            var modLoaderMod = ModLoader.GetMod("ModLoader"); //modmodloadermodmodloadermodmodloader
+            int mysteryTile = modLoaderMod.TileType("MysteryTile");
+            int mysteryTile2 = modLoaderMod.TileType("PendingMysteryTile");
+
+            var tile = Main.tile[Player.tileTargetX, Player.tileTargetY];
+            if (tile != null)
+            {
+                if (tile.active() && tile.type != mysteryTile && tile.type != mysteryTile2)
+                {
+                    var modTile = TileLoader.GetTile(tile.type);
+                    if (modTile != null)
+                    {
+                        WMITF.MouseText = ModContent.GetInstance<Config>().DisplayTechnicalNames ? (modTile.mod.Name + ":" + modTile.Name) : modTile.mod.DisplayName;
+                    }
+                }
+                else
+                {
+                    var modWall = WallLoader.GetWall(tile.wall);
+                    if (modWall != null)
+                    {
+                        WMITF.MouseText = ModContent.GetInstance<Config>().DisplayTechnicalNames ? (modWall.mod.Name + ":" + modWall.Name) : modWall.mod.DisplayName;
+                    }
+                }
+            }
+
+            var mousePos = Main.MouseWorld;
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                var npc = Main.npc[i];
+                if (mousePos.Between(npc.TopLeft, npc.BottomRight))
+                {
+                    var modNPC = NPCLoader.GetNPC(npc.type);
+                    if (modNPC != null && npc.active && !NPCID.Sets.ProjectileNPC[npc.type])
+                    {
+                        WMITF.MouseText = ModContent.GetInstance<Config>().DisplayTechnicalNames ? (modNPC.mod.Name + ":" + modNPC.Name) : modNPC.mod.DisplayName;
+                        WMITF.SecondLine = true;
+                        break;
+                    }
+                }
+            }
+            if (WMITF.MouseText != String.Empty && Main.mouseText)
+            {
+                WMITF.SecondLine = true;
+            }
+        }
+    }
+}

0b541ca4969419e731846af3eecef600dc17b233
Pop000100 <Pop000100@outlook.com>
2021-11-07 12:34:22 -0900

    Fix Unloaded tiles
    - I missed understood the mystery tile code
    - Added a logging function
    - Optimized the modloader stuff

--- WMITF.cs
+++ WMITF.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace WMITF
@@ -8,6 +9,11 @@ namespace WMITF
     {
         static public ModKeybind ToggleTooltipsHotkey;
         static public ModKeybind TechnicalNamesHotkey;
+        static public int unloadedItemType;
+        static public int aprilFoolsItemType;
+        static public int unloadedTileType1;
+        static public int unloadedTileType2;
+        static public int unloadedTileType3;
 
         public WMITF()
         {
@@ -18,6 +24,27 @@ namespace WMITF
         {
             ToggleTooltipsHotkey = KeybindLoader.RegisterKeybind(this, "Tile/NPC Mod Tooltip", "OemQuestion");
             TechnicalNamesHotkey = KeybindLoader.RegisterKeybind(this, "Technical Names", "N");
+        }
+
+        public override void PostSetupContent()
+        {
+            bool success;
+            ModLoader.TryGetMod("ModLoader", out Mod modLoaderMod);
+            success = modLoaderMod.TryFind<ModItem>("UnloadedItem", out ModItem unloadedItem);
+            if (success)
+                unloadedItemType = unloadedItem.Type;
+            success = modLoaderMod.TryFind<ModItem>("AprilFools", out ModItem aprilFoolsItem);
+            if (success)
+                aprilFoolsItemType = aprilFoolsItem.Type;
+            success = modLoaderMod.TryFind<ModTile>("UnloadedSolidTile", out ModTile unloadedTile1);
+            if (success)
+                unloadedTileType1 = unloadedTile1.Type;
+            success = modLoaderMod.TryFind<ModTile>("UnloadedNonSolidTile", out ModTile unloadedTile2);
+            if (success)
+                unloadedTileType2 = unloadedTile2.Type;
+            success = modLoaderMod.TryFind<ModTile>("UnloadedSemiSolidTile", out ModTile unloadedTile3);
+            if (success)
+                unloadedTileType3 = unloadedTile3.Type;
         }
 
         public override void Unload()
@@ -32,8 +59,28 @@ namespace WMITF
 			return date.Month == 4 && date.Day <= 2;
 		}
 
-		// Hamstar's Mod Helpers integration
-		public static string GithubUserName { get { return "goldenapple3"; } }
+        public static bool isUnloadedTile(Tile tile)
+        {
+            if (tile.IsActive && tile != null)
+            {
+                if (tile.type == unloadedTileType1 || tile.type == unloadedTileType2 || tile.type == unloadedTileType3)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        public static void Log(object message)
+        {
+            ModContent.GetInstance<WMITF>().Logger.Info(message);
+        }
+
+        // Hamstar's Mod Helpers integration
+        public static string GithubUserName { get { return "goldenapple3"; } }
 		public static string GithubProjectName { get { return "WMITF"; } }
 	}
 }
